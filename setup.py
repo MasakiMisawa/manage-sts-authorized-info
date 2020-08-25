7d960532d7bb4906ebdb72a3383bce5ade4a4f5d
MasakiMisawa <>
2020-08-25 12:52:05 +0900

    WIP create setup.py and test

--- setup.py
+++ setup.py
@@ -1,1 +1,231 @@
 import yaml
+import logging
+import os
+import datetime
+
+## const value
+SETUP_CONFIG_FILE_PATH = "config/setup-config.yaml"
+SETUP_LOG_FILE_PATH = "logs/setup.log"
+BASH_LOGIN_SHELL_SETTING_FILE_PATH = "$HOME/.bashrc"
+ZSH_LOGIN_SHELL_SETTING_FILE_PATH = "$HOME/.zshrc"
+REGISTER_STS_ASSUMED_ROLE_TEMPLATE_FILE_PATH = "template/register_sts_assumed_role.tmpl"
+REPLACEMENT_STRING_CONFIG_FILE_PATH = "$REPLACEMENT_STRING_CONFIG_FILE_PATH"
+REPLACEMENT_STRING_REGISTER_PROFILE = "$REPLACEMENT_STRING_REGISTER_PROFILE"
+REPLACEMENT_STRING_REGION_NAME = "$REPLACEMENT_STRING_REGION_NAME"
+REPLACEMENT_STRING_OUTPUT_FORMAT = "$REPLACEMENT_STRING_OUTPUT_FORMAT"
+REPLACEMENT_STRING_CHANGE_LOG_FILE_PATH = "$REPLACEMENT_STRING_CHANGE_LOG_FILE_PATH"
+REGISTER_STS_ASSUMED_ROLE_START_SIGNAL = (
+    "###### register_sts_assumed_role starts here ######"
+)
+REGISTER_STS_ASSUMED_ROLE_END_SIGNAL = (
+    "###### register_sts_assumed_role ends here ######"
+)
+
+
+class SetupConfigVO:
+    def __init__(
+        self, config_file_path, profile_name, region, output, change_log_file_path
+    ):
+        """
+        Parameters
+        ----------
+        config_file_path : str
+            AWS CLI config file path.
+        profile_name : str
+            default profile name.
+        region : str
+            default region name.
+        output : str
+            default output format.
+        change_log_file_path : str
+            file path for config file change log.
+        """
+        self.config_file_path = config_file_path
+        self.profile_name = profile_name
+        self.region = region
+        self.output = output
+        self.change_log_file_path = change_log_file_path
+
+
+def load_setup_config(yaml_file_path):
+    """
+    Load setup config from yaml file.
+
+    Parameters
+    ----------
+    yaml_file_path : str
+        setup-config yaml file path.
+
+    Returns
+    -------
+    setup_config : SetupConfigVO
+        loaded config detail value object.
+    """
+    with open(yaml_file_path, "r") as config_file:
+        config = yaml.load(config_file, Loader=yaml.SafeLoader)["setup"]
+
+    register_profile = config["register_profile"]
+    return SetupConfigVO(
+        config["config_file"]["file_path"],
+        register_profile["profile_name"]["default"],
+        register_profile["region"]["default"],
+        register_profile["output"]["default"],
+        config["change_log"]["file_path"],
+    )
+
+
+def initialize_logger_setting():
+    """
+    Initialize logger setting.
+
+    Returns
+    -------
+    logger : logger
+        logging.logger object.
+    """
+    if not os.path.isdir("logs"):
+        os.makedirs("logs")
+    logging.basicConfig(filename=SETUP_LOG_FILE_PATH)
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    return logger
+
+
+def get_login_shell_setting_file_path(login_shell_path):
+    """
+    Get your local login shell setting file path.
+
+    Parameters
+    ----------
+    login_shell_path : str
+        Your local login shell path.
+
+    Returns
+    -------
+    login_shell_setting_file_path : str
+        login shell setting file path.
+    """
+    if login_shell_path is None:
+        return None
+    elif login_shell_path.endswith("bash"):
+        return BASH_LOGIN_SHELL_SETTING_FILE_PATH.replace("$HOME", os.environ["HOME"])
+    elif login_shell_path.endswith("zsh"):
+        return ZSH_LOGIN_SHELL_SETTING_FILE_PATH.replace("$HOME", os.environ["HOME"])
+    else:
+        return None
+
+
+def replace_replacement_string(
+    template_string,
+    config_file_path,
+    profile_name,
+    region,
+    output,
+    change_log_file_path,
+):
+    """
+    Generate register-sts-assumed-role function string.
+
+    Parameters
+    ----------
+    template_string : str
+        register-sts-assumed-role function template string.
+    config_file_path : str
+        AWS CLI config file path.
+    profile_name : str
+        default profile name.
+    region : str
+        default region name.
+    output : str
+        default output format.
+    change_log_file_path : str
+        file path for config file change log.
+
+    Returns
+    -------
+    str_register_sts_assumed_role : str
+        register-sts-assumed-role function string.
+    """
+    return (
+        template_string.replace(REPLACEMENT_STRING_CONFIG_FILE_PATH, config_file_path)
+        .replace(REPLACEMENT_STRING_REGISTER_PROFILE, profile_name)
+        .replace(REPLACEMENT_STRING_REGION_NAME, region)
+        .replace(REPLACEMENT_STRING_OUTPUT_FORMAT, output)
+        .replace(REPLACEMENT_STRING_CHANGE_LOG_FILE_PATH, change_log_file_path)
+    )
+
+
+def generate_register_sts_assumed_role_template(template_file_path, setup_config):
+    """
+    Generate register-sts-assumed-role function string.
+
+    Parameters
+    ----------
+    template_file_path : str
+        register-sts-assumed-role template file path.
+    setup_config : SetupConfigVO
+        loaded config detail value object.
+
+    Returns
+    -------
+    str_register_sts_assumed_role : str
+        register-sts-assumed-role function string.
+    """
+    with open(template_file_path) as f:
+        return replace_replacement_string(
+            f.read(),
+            setup_config.config_file_path,
+            setup_config.profile_name,
+            setup_config.region,
+            setup_config.output,
+            setup_config.change_log_file_path,
+        )
+
+
+def reload_login_shell_setting_file(login_shell_setting_file_path):
+    """
+    Run the source of linux commands in the login shell setting file.
+
+    Parameters
+    ----------
+    login_shell_setting_file_path : ztr
+        login shell setting file path.
+    """
+    pass
+
+
+def setup_register_sts_assumed_role(setup_config, logger):
+    """
+    Setup register_sts_assumed_role function to login shell setting file.
+
+    Parameters
+    ----------
+    setup_config : SetupConfigVO
+        loaded config detail value object.
+    logger : logger
+        logging.logger object.
+    """
+    login_shell_path = os.environ["SHELL"]
+    login_shell_setting_file_path = get_login_shell_setting_file_path(login_shell_path)
+    if login_shell_setting_file_path is None:
+        logger.error(
+            datetime.datetime.now().isoformat()
+            + " Login shell not supported error. login_shell_path = "
+            + login_shell_path
+        )
+        print("Sorry. the only supported login shells are bash and zsh.")
+        return
+
+    str_register_sts_assumed_role = generate_register_sts_assumed_role_template(
+        REGISTER_STS_ASSUMED_ROLE_TEMPLATE_FILE_PATH, setup_config
+    )
+    # print(str_register_sts_assumed_role)
+    reload_login_shell_setting_file(login_shell_setting_file_path)
+    print("setup finished.")
+
+
+if __name__ == "__main__":
+    config = load_setup_config(SETUP_CONFIG_FILE_PATH)
+    setup_register_sts_assumed_role(
+        config, initialize_logger_setting(),
+    )
--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,15 +1,123 @@
 import unittest
 import setup
+import logging
+import os
+import yaml
+
+TEST_RESULT_LOG_FILE_PATH = "tests/logs/test_result.log"
+SETUP_CONFIG_FILE_PATH = "config/setup-config.yaml"
+AWS_ALL_REGIONS = [
+    "us-east-2",
+    "us-east-1",
+    "us-west-1",
+    "us-west-2",
+    "af-south-1",
+    "ap-east-1",
+    "ap-south-1",
+    "ap-northeast-3",
+    "ap-northeast-2",
+    "ap-northeast-1",
+    "ap-southeast-1",
+    "ap-southeast-2",
+    "ca-central-1",
+    "eu-central-1",
+    "eu-west-1",
+    "eu-west-2",
+    "eu-south-1",
+    "eu-west-3",
+    "eu-north-1",
+    "me-south-1",
+    "sa-east-1",
+]
+CLI_ALL_OUTPUT_FORMATS = ["json", "yaml", "text", "table"]
+BASH_LOGIN_SHELL_SETTING_FILE_PATH = "$HOME/.bashrc"
+ZSH_LOGIN_SHELL_SETTING_FILE_PATH = "$HOME/.zshrc"
 
 
 class TestSetup(unittest.TestCase):
+    logger = None
+
     @classmethod
     def setUpClass(cls):
-        pass
+        cls.__initialize_logger()
+
+    @classmethod
+    def __initialize_logger(cls):
+        if not os.path.isdir("tests/logs"):
+            os.makedirs("tests/logs")
+        global logger
+        logging.basicConfig(filename=TEST_RESULT_LOG_FILE_PATH)
+        logger = logging.getLogger()
+        logger.setLevel(logging.INFO)
+
+    def test_load_setup_config_all_values_must_be_available(self):
+        ## given
+        with open(SETUP_CONFIG_FILE_PATH, "r") as config_file:
+            config = yaml.load(config_file, Loader=yaml.SafeLoader)["setup"]
+            config_file_path = config["config_file"]["file_path"]
+
+            register_profile = config["register_profile"]
+            profile_name = register_profile["profile_name"]["default"]
+            region = register_profile["region"]["default"]
+            output = register_profile["output"]["default"]
+
+            change_log_file_path = config["change_log"]["file_path"]
+
+        ## when
+        config = setup.load_setup_config(setup.SETUP_CONFIG_FILE_PATH)
+
+        ## then
+        self.assertTrue(len(config.config_file_path) > 0)
+        self.assertEqual(config.config_file_path, config_file_path)
+        self.assertTrue(len(config.profile_name) > 0)
+        self.assertEqual(config.profile_name, profile_name)
+        self.assertTrue(len(config.region) > 0)
+        self.assertEqual(config.region, region)
+        self.assertTrue(len(config.output) > 0)
+        self.assertEqual(config.output, output)
+        self.assertTrue(len(config.change_log_file_path) > 0)
+        self.assertEqual(config.change_log_file_path, change_log_file_path)
+
+    def test_load_setup_config_expected_value(self):
+        ## when
+        config = setup.load_setup_config(SETUP_CONFIG_FILE_PATH)
+
+        ## then
+        self.assertIn(config.region, AWS_ALL_REGIONS)
+        self.assertIn(config.output, CLI_ALL_OUTPUT_FORMATS)
+
+    def test_get_login_shell_setting_file_path_expected_value(self):
+        ## given
+        login_shells = [
+            {
+                "login_shell_path": "/bin/bash",
+                "expected_value": BASH_LOGIN_SHELL_SETTING_FILE_PATH.replace(
+                    "$HOME", os.environ["HOME"]
+                ),
+            },
+            {
+                "login_shell_path": "/usr/local/bin/zsh",
+                "expected_value": ZSH_LOGIN_SHELL_SETTING_FILE_PATH.replace(
+                    "$HOME", os.environ["HOME"]
+                ),
+            },
+            {"login_shell_path": "/bin/tcsh", "expected_value": None},
+            {"login_shell_path": None, "expected_value": None},
+        ]
+
+        for login_shell in login_shells:
+            ## when
+            login_shell_setting_file_path = setup.get_login_shell_setting_file_path(
+                login_shell["login_shell_path"]
+            )
+
+            ## then
+            self.assertEqual(
+                login_shell_setting_file_path, login_shell["expected_value"]
+            )
 
     @classmethod
     def tearDownClass(cls):
-        ## todo
         pass
 
 
